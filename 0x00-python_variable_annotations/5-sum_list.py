b6fcc21083881067e24624d86919745ad693cea9
Buguster <your.email@example.com>
2024-05-02 22:02:13 +0100

    Python Annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""returns the sum of an input_list floats as a float"""
+
+
+def sum_list(input_list: list) -> float:
+    """The function Logic"""
+    return sum(input_list)
