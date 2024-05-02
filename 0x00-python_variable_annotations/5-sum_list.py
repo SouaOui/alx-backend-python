415d484c8c59aafc0af22511e12395d3ca307652
Buguster <your.email@example.com>
2024-05-02 22:05:02 +0100

    Python Annotations

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """returns the sum of an input_list floats as a float"""
+from typing import List
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: List[float]) -> float:
     """The function Logic"""
     return float(sum(input_list))
