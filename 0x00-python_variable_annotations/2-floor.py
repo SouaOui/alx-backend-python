ac47d5fbc5d17dc91ac4935dd179ecb2d2721846
Buguster <your.email@example.com>
2024-05-02 21:52:29 +0100

    Python Annotations

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-import math
 """Floor function with annotations"""
 
 
+import math
 def floor(num: float) -> int:
     """Return the floor of a number."""
     return math.floor(num)
