0ba476cd97089258bdc950128970983bbddbce5c
Buguster <your.email@example.com>
2024-05-02 22:52:52 +0100

    Python Annotations

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,7 +5,6 @@ from typing import Tuple, Any
 
 def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> Tuple[Any, ...]:
     """Making changes after checking with mypy"""
-
     zoomed_in = tuple(
         item for item in lst
         for i in range(factor)
