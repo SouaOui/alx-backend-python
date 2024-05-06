eb0642e539644a0bf50ca34bb23b3468f6054c7f
Buguster <your.email@example.com>
2024-05-06 11:24:09 +0100

    Async Python

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""coroutine function asynchronous named wait_random"""
+import asyncio
+import random
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_random(max_delay: int = 10) -> float:
+    """task_wait_random function"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """
+    Take the code from wait_n and alter it into a new function task_wait_n.
+    The code is nearly identical to wait_n except task_wait_random is being called.
+    """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*tasks)
+    return sorted(results)
