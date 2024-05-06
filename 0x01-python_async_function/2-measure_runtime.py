6b1120afb5b52f4683eff86d21cfdb1b2fb79935
Buguster <your.email@example.com>
2024-05-06 11:13:09 +0100

    Async Python

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""coroutine function asynchronous named wait_random"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int = 10) -> float:
+    """Measure the runtime"""
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.time() - start_time
+    return total_time / n
