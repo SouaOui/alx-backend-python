93d6d59a70c7f204343138803b4cc0d17287c877
Buguster <your.email@example.com>
2024-05-06 10:46:15 +0100

    Async Python

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """coroutine function asynchronous named wait_random"""
+import asyncio
 import random
 
 
 async def wait_random(max_delay=10):
     """the function logic"""
-    return random.uniform(0, max_delay)
+    wait_time = random.uniform(0, max_delay)
+    await asyncio.sleep(wait_time)
+    return wait_time
