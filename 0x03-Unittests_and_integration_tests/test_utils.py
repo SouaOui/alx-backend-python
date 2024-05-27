0faa6d980de343cb6df1c406a2cbb453e3b8bd6c
SouaOui <your.email@example.com>
2024-05-27 12:38:31 +0100

    adding annotations in python

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,20 +8,7 @@ from typing import Dict, Tuple, Union
 
 
 class TestStringMethods(unittest.TestCase):
-    """
-    implementing the first unittest to test the method of access_nested_map
-        unittest: For creating and running tests.
-        parameterized: For running the same test with different parameters.
-
-    TestAccessNestedMap inherits from unittest.TestCase.
-        @parameterized.expand decorator is used to provide different
-        sets of inputs and expected outputs.
-
-    test_access_nested_map takes nested_map, path, and expected as arguments.
-    It asserts that the result of
-    access_nested_map(nested_map, path) is equal to expected.
-    using the assertEqual() method along with the function to test
-    """
+    """Tests the `access_nested_map` function."""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -33,7 +20,6 @@ class TestStringMethods(unittest.TestCase):
             path: Tuple[str],
             expected: Union[Dict, int],
     ) -> None:
-        """Tests `access_nested_map`'s output."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
 
