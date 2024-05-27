274caf6b2c542f84d8fc4ae4ba683d6816212a42
SouaOui <your.email@example.com>
2024-05-27 12:39:45 +0100

    adding annotations in python

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,12 +2,16 @@
 """A module for testing the utils module.
 """
 import unittest
-from parameterized import parameterized
-from utils import access_nested_map
 from typing import Dict, Tuple, Union
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+
+from utils import (
+    access_nested_map
+)
 
 
-class TestStringMethods(unittest.TestCase):
+class TestAccessNestedMap(unittest.TestCase):
     """Tests the `access_nested_map` function."""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -20,8 +24,5 @@ class TestStringMethods(unittest.TestCase):
             path: Tuple[str],
             expected: Union[Dict, int],
     ) -> None:
+        """Tests `access_nested_map`'s output."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
-
-
-if __name__ == "__main__":
-    unittest.main()
